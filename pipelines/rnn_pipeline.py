d0fd2bb36e4c136a41d85644f606a0fef18d3e46
renato boemer <boemer00@gmail.com>
2023-10-12 11:02:24 +0100

    rmse model running from terminal and logging mlflow

--- pipelines/rnn_pipeline.py
+++ pipelines/rnn_pipeline.py
@@ -25,46 +25,6 @@ def evaluate_model(model, X_test, y_test):
     print(f'Test RMSE: {rmse:.4f}')
     return rmse
 
-
-# def main():
-#     # 1. Data Loading
-#     raw_data = load_train_data()
-
-#     # 2. Data Transformation
-#     rul_adder = RULAdder()
-#     raw_data_with_rul = rul_adder.transform(raw_data)
-
-#     constant_column_dropper = ConstantColumnDropper()
-#     transformed_data = constant_column_dropper.fit_transform(raw_data_with_rul)
-
-#     sequence_creator = SequenceCreator(sequence_length=50)
-#     sequences, labels = sequence_creator.transform_with_labels(transformed_data)
-
-#     # 3. Data Splitting and Scaling
-#     X_train, X_test, y_train, y_test = train_test_split(sequences, labels, test_size=0.2, random_state=42)
-
-#     data_scaler = DataScaler()
-#     X_train_scaled = data_scaler.fit_transform(X_train)
-#     X_test_scaled = data_scaler.transform(X_test)
-
-#     # 4. Model Initialisation
-#     input_shape = (X_train_scaled.shape[1], X_train_scaled.shape[2])
-#     model = initialize_model(input_shape)
-
-#     # 5. Model Training
-#     trained_model = train_model(model, X_train_scaled, y_train, X_test_scaled, y_test)
-
-#     # 6. Model Evaluation
-#     evaluate_model(trained_model, X_test_scaled, y_test)
-
-#     # 7. Save the trained model with timestamped filename
-#     save_dir = './models/saved'
-#     os.makedirs(save_dir, exist_ok=True)
-
-#     timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-#     save_path = f'{save_dir}/model_{timestamp}.keras'
-#     trained_model.save(save_path)
-
 def main():
     mlflow.set_experiment('default_experiment')
 
