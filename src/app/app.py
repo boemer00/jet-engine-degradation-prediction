6118c1554651cfbe36309180e7b1c891ad1e194f
renato boemer <boemer00@gmail.com>
2023-11-10 17:26:17 +0000

    fast api accepts txt files to predict

--- src/app/app.py
+++ src/app/app.py
@@ -1,13 +1,9 @@
 import os
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+import pandas as pd
+from fastapi import FastAPI, HTTPException, UploadFile, File
 import tensorflow as tf
 import numpy as np
 from src.transform.data_transformation import RULAdder, ConstantColumnDropper, SequenceCreator, DataScaler
-
-# Define a Pydantic model for the input data
-class Features(BaseModel):
-    features: list
 
 # Initialise the FastAPI application
 app = FastAPI()
@@ -28,13 +24,43 @@ sequence_creator = SequenceCreator(sequence_length=sequence_length)
 data_scaler = DataScaler()
 
 @app.post('/predict')
-def predict(features: Features):
+async def predict(file: UploadFile = File(...)):
     try:
-        # Convert features to numpy array
-        feature_array = np.array(features.features)
+        # Define column names
+        index_names = ['Engine', 'Cycle']
+        setting_names = ['Setting 1', 'Setting 2', 'Setting 3']
+        sensor_names = ['Fan Inlet Temperature (◦R)',
+               'LPC Outlet Temperature (◦R)',
+               'HPC Outlet Temperature (◦R)',
+               'LPT Outlet Temperature (◦R)',
+               'Fan Inlet Pressure (psia)',
+               'Bypass-Duct Pressure (psia)',
+               'HPC Outlet Pressure (psia)',
+               'Physical Fan Speed (rpm)',
+               'Physical Core Speed (rpm)',
+               'Engine Pressure Ratio (P50/P2)',
+               'HPC Outlet Static Pressure (psia)',
+               'Ratio of Fuel Flow to Ps30 (pps/psia)',
+               'Corrected Fan Speed (rpm)',
+               'Corrected Core Speed (rpm)',
+               'Bypass Ratio',
+               'Burner Fuel-Air Ratio',
+               'Bleed Enthalpy',
+               'Required Fan Speed',
+               'Required Fan Conversion Speed',
+               'High-Pressure Turbines Cool Air Flow',
+               'Low-Pressure Turbines Cool Air Flow',
+               'Sensor 26',
+               'Sensor 27']
 
+        col_names = index_names + setting_names + sensor_names
+
+        # Read the uploaded file into a DataFrame
+        df = pd.read_csv(file.file, sep='\s+', header=None, names=col_names)
+
+        # Perform the necessary transformations on the DataFrame
         # 1. Add RUL
-        feature_array_with_rul = rul_adder.transform(feature_array)
+        feature_array_with_rul = rul_adder.transform(df)
 
         # 2. Drop constant columns
         transformed_data = constant_column_dropper.transform(feature_array_with_rul)
@@ -60,46 +86,3 @@ def predict(features: Features):
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=5000)
-
-
-
-# from fastapi import FastAPI, HTTPException
-# from pydantic import BaseModel
-# import tensorflow as tf
-# import numpy as np
-
-# # Define a Pydantic model for the input data
-# class Features(BaseModel):
-#     features: list
-
-# # Initialise the FastAPI application
-# app = FastAPI()
-
-# # Load the trained model
-# model = tf.keras.models.load_model('/jet-engine/model.keras')
-
-# @app.post('/predict')
-# def predict(features: Features):
-#     try:
-#         # Convert features to numpy array
-#         feature_array = np.array(features.features)
-
-#         # Perform any necessary reshaping, preprocessing here
-#         # For example, if your model expects data of a certain shape
-#         # feature_array = feature_array.reshape(expected_shape)
-
-#         # Generate predictions
-#         predictions = model.predict(feature_array)
-
-#         # Convert predictions to a list for JSON response
-#         predictions_list = predictions.tolist()
-
-#         # Send back the predictions as a response
-#         return {"predictions": predictions_list}
-
-#     except Exception as e:
-#         raise HTTPException(status_code=500, detail=str(e))
-
-# if __name__ == '__main__':
-#     import uvicorn
-#     uvicorn.run(app, host='0.0.0.0', port=5000)
